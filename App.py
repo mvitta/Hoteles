49283b704835da541cdcf8a99213446ed726b972
mvitta <mikesystem@hotmail.com>
2021-10-20 22:01:00 -0500

    sesiones con base de datos creadas

--- App.py
+++ App.py
@@ -8,9 +8,14 @@ import sqlite3
 
 app = Flask(__name__)
 app.secret_key = os.urandom(34)
-DATABASE = "registroUsuarios.db"
+DATABASE_USUARIO = "hotel.db"
 
-# retorna verdadero solo si el usuario existe
+
+def conexionBaseDeDatos():
+    db = getattr(g, '_database_USUARIO', None)
+    if db is None:
+        db = g._database_USUARIO = sqlite3.connect(DATABASE_USUARIO)
+    return db
 
 
 def usuarioExiste(cedula):
@@ -39,27 +44,64 @@ def correoExiste(correo):
     return False
 
 
-def buscarCredenciales(correo):
+def buscarCredencialesUsuario(correo):
     conexion = conexionBaseDeDatos()
     cur = conexion.cursor()
     sql = "SELECT correo, contra FROM usuarios WHERE correo=?"
     cur.execute(sql, [correo])
     conexion.commit()
-    info = cur.fetchone()
+    infoUser = cur.fetchone()
     cur.close()
-    return info
+    return infoUser
 
 
-def conexionBaseDeDatos():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+def buscarCredencialesAdmin(correo):
+    conexion = conexionBaseDeDatos()
+    cur = conexion.cursor()
+    sql = "SELECT id_administrador, correo, contra FROM administrador WHERE correo=?"
+    cur.execute(sql, [correo])
+    conexion.commit()
+    infoAdmin = cur.fetchone()
+    cur.close()
+    return infoAdmin
+
+
+def buscarCredencialesSuperAdmin(correo):
+    conexion = conexionBaseDeDatos()
+    cur = conexion.cursor()
+    sql = "SELECT correo, contra FROM superadministrador WHERE correo=?"
+    cur.execute(sql, [correo])
+    conexion.commit()
+    infoSuperAdmin = cur.fetchone()
+    cur.close()
+    return infoSuperAdmin
+
+
+def listaDeHabitaciones():
+    conexion = conexionBaseDeDatos()
+    cur = conexion.cursor()
+    sql1 = "SELECT * FROM habitaciones"
+    cur.execute(sql1)
+    conexion.commit()
+    infoHabitaciones = cur.fetchall()
+    cur.close()
+    return infoHabitaciones
+
+
+def numeroDeRegistro():
+    conexion = conexionBaseDeDatos()
+    cur = conexion.cursor()
+    sql2 = "SELECT count(*) FROM habitaciones"
+    cur.execute(sql2)
+    conexion.commit()
+    filas = cur.fetchone()
+    cur.close()
+    return filas
 
 
 @app.teardown_appcontext
 def cerrarConexion(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, '_database_USUARIO', None)
     if db is not None:
         db.close()
 
@@ -85,28 +127,21 @@ def registro():
     metodo = request.method
     print(metodo)
     if metodo == "POST":
-
         cedula = request.form.get('Cedula')
         _nombre = request.form.get('Nombre')
         _apellido = request.form.get('Apellido')
         _correo = request.form.get('Correo')
         _contra = generate_password_hash(request.form.get('Contra'))
-
-        print(cedula, _nombre, _apellido, _correo, _contra)
-
         usuarioBD = usuarioExiste(cedula)
         correoBD = correoExiste(_correo)
-
-        print(usuarioBD, correoBD)
-
         if usuarioBD and correoBD:
-            # si el usuario muestra error
             return "El Usuario Existe y el correo ya se encuentra registrado"
         elif usuarioBD:
             return "ya hay un usuario con ese numer de cedula"
         elif correoBD:
             return "el correo ya se encuentra registrado"
         else:
+            # realiza la conexion eh ingresa la informacion si todo esta correcto
             conexion = conexionBaseDeDatos()
             cur = conexion.cursor()
             sql = "INSERT OR IGNORE INTO usuarios (cedula, nombre, apellido, correo, contra) VALUES (?, ?, ?, ?, ?)"
@@ -123,32 +158,41 @@ def registro():
 def hacerlogin():
     try:
         metodo = request.method
-        print(metodo)
         if metodo == 'POST':
             us = request.form['usuario']
             co = request.form['contra']
-
-            creden = buscarCredenciales(us)
-
-            if creden is None:
-                return "error en la contrasena"
+            pos = us.find("@")
+            valida = us[pos:len(us)]
+            if valida == "@cuc.com":
+                creden = buscarCredencialesUsuario(us)
+                if creden is None:
+                    return "error en la contrasena o usuario"
+                else:
+                    if check_password_hash(creden[1], co):
+                        session.clear()
+                        session['usuario'] = us
+                        return redirect(url_for('DashboardUsuariofinal'))
+            elif valida == "@admin.com":
+                creden = buscarCredencialesAdmin(us)
+                if creden is None:
+                    return "error en la contrasena o usuario"
+                else:
+                    if check_password_hash(creden[2], co):
+                        session.clear()
+                        session['admin'] = us
+                        session['id'] = creden[0]
+                        return redirect(url_for('dashboardAdministrador'))
+            elif valida == "@superadmin.com":
+                creden = buscarCredencialesSuperAdmin(us)
+                if creden is None:
+                    return "error en la contrasena o usuario"
+                else:
+                    if check_password_hash(creden[1], co):
+                        session.clear()
+                        session['super'] = us
+                        return redirect(url_for('dashboardSuperAdministrador'))
             else:
-                if check_password_hash(creden[1], co):
-                    session.clear()
-                    session['usuario'] = us
-                    return redirect(url_for('DashboardUsuariofinal'))
-
-            # if us == "admin" and co == "1234":
-
-            #     print("CONTRASEÑA CIFRADA", co)
-            #     session['admin'] = us
-            #     return redirect(url_for('dashboardAdministrador'))
-            # elif us == "superadmin" and co == "1234":
-            #     session['super'] = us
-            #     return redirect(url_for('dashboardSuperAdministrador'))
-            # elif us == "user" and co == "1234":
-            #     session['usuario'] = us
-            #     return redirect(url_for('DashboardUsuariofinal'))
+                return "El correo no es valido para iniciar sersion"
         else:
             return "bad request"
 
@@ -168,14 +212,40 @@ def dashboardAdministrador():
         return "No tienes los permisos para entrar al Administrador, inicia session con tu cuenta"
 
 
-@app.route('/dashboardAdministrador/crearHabitacion')
+@app.route('/dashboardAdministrador/crearHabitacion', methods=['GET', 'POST'])
 def crearHabitacionAdmin():
-    return render_template('CrearHabitacionAdmin.html')
+    metodo = request.method
+    print(metodo)
+    if metodo == 'POST':
+        habitacion = request.form.get('habitacion')
+        tipoHabitacion = request.form.get('tipoHabitacion')
+        sabanas = request.form.get('sabanas')
+        numeroCamas = request.form.get('numeroCamas')
+        servicios = request.form.get('servicios')
+        print(habitacion, tipoHabitacion, sabanas, numeroCamas, servicios)
+        # idAdmin = session['id']
+        # print(idAdmin)
+        conexion = conexionBaseDeDatos()
+        cur = conexion.cursor()
+        sql = "INSERT INTO habitaciones (id_administrador, tipo_habitacion, tipo_cama, sabanas, numero_camas, servicios) VALUES (?, ?, ?, ?, ?, ?)"
+        cur.execute(
+            sql, [1, habitacion, tipoHabitacion, sabanas, numeroCamas, servicios])
+        conexion.commit()
+        cur.close()
+
+        return redirect(url_for('crearHabitacionAdmin'))
+    else:
+        return render_template('CrearHabitacionAdmin.html')
 
 
-@app.route('/dashboardAdministrador/editareliminar')
+@app.route('/dashboardAdministrador/editareliminar', methods=['GET', 'POST'])
 def EditarEliminarAdmin():
-    return render_template('EditarEliminarAdmin.html')
+    metodo = request.method
+    if metodo == "POST":
+        pass
+    if metodo == "GET":
+        datos = listaDeHabitaciones()
+        return render_template('EditarEliminarAdmin.html', datos=datos, filas=numeroDeRegistro())
 
 
 # ----------- USUARIO FINAL --------------
@@ -190,7 +260,8 @@ def DashboardUsuariofinal():
 
 @app.route('/DashboardUsuariofinal/buscarHabitacion')
 def buscarHabitacion():
-    return render_template('BuscarHabitacion.html')
+    datos = listaDeHabitaciones()
+    return render_template('BuscarHabitacion.html', datos=datos, filas=numeroDeRegistro())
 
 
 @app.route('/DashboardUsuariofinal/reserva', methods=['GET', 'POST'])
